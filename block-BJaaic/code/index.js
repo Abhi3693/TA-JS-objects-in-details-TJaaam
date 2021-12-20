6142c122e4cd4e78539abff2ee35e56131fdee60
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-12-20 11:41:46 +0530

    Added Change in Inheritance Assignment

--- block-BJaaic/code/index.js
+++ block-BJaaic/code/index.js
@@ -1,41 +1,16 @@
 let animalMethods = {
-    summury: function () {
-        return `I live in ${this.location} and I have ${this.noOfLegs} legs`
+
+    eat: function(){
+        console.log(`I live in ${this.location} and I can eat`);
     },
-    eat: function() {
-        console.log(`I live in ${this.location} and I can eat`)
-    }, 
-    changeLocation: function(newLocation) {
-        this.location = newLocation
-    }
+    changeLocation: function(newLocation){
+        this.location = newLocation;
+    },
+    summary: function(){
+        console.log(`I live in ${this.location} and I have ${this.noOfLegs} legs`);
+    },
 }
 
-let dogMethods = {
-    bark: function () {
-        alert(`I am ${this.name} and I can bark 🐶`);
-    },
-    changeName: function (newName) {
-        this.name = newName;
-    },
-    changeColor: function (newColor) {
-        this.color = newColor;
-    },
-    summary : function() {
-        return `I am ${this.name} and I am of ${this.color} color. I can also bark`; 
-    }
-}
-
-let catMethods = {
-    meow: function () {
-        alert(`I am ${this.name} and I can do mewo meow 😹`);
-    },
-    changeName: function (newName) {
-        this.name = newName;
-    },
-    changeColor: function (newColor) {
-        this.color = newColor;
-    },
-}
 
 function createAnimal(location, noOfLegs) {
     let obj = Object.create(animalMethods);
@@ -44,26 +19,58 @@ function createAnimal(location, noOfLegs) {
     return obj;
 }
 
-function createDog(location, noOfLegs,name,color) {
-    let obj = Object.create(dogMethods);
-    obj.location = location;
-    obj.noOfLegs = noOfLegs;
-    obj.name = name;
-    obj.color = color;
-    return obj;
+
+let catMethods = {
+    meow: function(){
+        console.log(`I live in ${this.location} and I can eat`);
+    },
+    changeName: function(name){
+        this.name = name;
+    },
+    changeColorOfEyes: function(newColor){
+        this.colorOfEye = newColor;
+    },
+    summary: function () {
+        console.log(`I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can also do meow meow`);
+    }
 }
 
-function createCat(location, noOfLegs,name,colorOfEye) {
-    let obj = Object.create(catMethods);
-    obj.location = location;
-    obj.noOfLegs = noOfLegs;
+
+function createCat(location, noOfLegs, name, colorOfEye) {
+    let obj = createAnimal(location, noOfLegs);
+    Object.setPrototypeOf(obj, catMethods);
     obj.name = name;
     obj.colorOfEye = colorOfEye;
     return obj;
 }
 
-Object.setPrototypeOf(dogMethods, animalMethods);
-Object.setPrototypeOf(catMethods, animalMethods);
 
-let dog = new createDog("Morochi", 4, "Tyson", "Black");
-let cat = new createCat("Pune", 2, "Manju", "Brown");
+let dogMethods = {
+    bark: function(){
+        console.log(`I live in ${this.location} and I can eat`);
+    },
+    changeName: function(name) {
+        this.name = name;
+    },
+    changeColor: function(newColor){
+        this.color = newColor;
+    },
+    summary: function() {
+        console.log(`I am ${this.name} and I am of ${this.color} color. I can also bark`);
+    }
+}
+
+function createDog(location, noOfLegs, name, color) {
+    let obj = createAnimal(location, noOfLegs);
+    Object.setPrototypeOf(obj, dogMethods);
+    obj.name = name;
+    obj.color = color;
+    return obj;
+}
+
+Object.setPrototypeOf(dogMethods, animalMethods)
+Object.setPrototypeOf(catMethods, animalMethods)
+
+
+let cat =  createCat("Pune", 2, "Manju", "Brown");
+let dog =  createDog("Morochi", 4, "Tyson", "Black");
